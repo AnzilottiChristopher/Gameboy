b00c15f14dc29f6a3ed7dfbd1651995401d5c3fd
Christopher Anzilotti <anzilottichr@gmail.com>
2026-01-13 22:14:12 -0500

    Added Inc/Dec Opcodes

--- core/target/debug/deps/gb_core-ec1649895f553902.d
+++ core/target/debug/deps/gb_core-ec1649895f553902.d
@@ -1,7 +1,8 @@
-C:\Users\anzil\dev\rust_projects\Gameboy\core\target\debug\deps\gb_core-ec1649895f553902.d: src\lib.rs src\cpu\mod.rs src\utils.rs
+C:\Users\anzil\dev\rust_projects\Gameboy\core\target\debug\deps\gb_core-ec1649895f553902.d: src\lib.rs src\cpu\mod.rs src\cpu\opcodes.rs src\utils.rs
 
-C:\Users\anzil\dev\rust_projects\Gameboy\core\target\debug\deps\libgb_core-ec1649895f553902.rmeta: src\lib.rs src\cpu\mod.rs src\utils.rs
+C:\Users\anzil\dev\rust_projects\Gameboy\core\target\debug\deps\libgb_core-ec1649895f553902.rmeta: src\lib.rs src\cpu\mod.rs src\cpu\opcodes.rs src\utils.rs
 
 src\lib.rs:
 src\cpu\mod.rs:
+src\cpu\opcodes.rs:
 src\utils.rs:
